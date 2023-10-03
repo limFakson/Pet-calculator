0c19aba01772162e0677fc5aedf7b5a74f52359b
limFakson <fakeyejoshua2005@gmail.com>
2023-10-03 11:42:23 +0100

    commit

--- files/cal.js
+++ files/cal.js
@@ -1,12 +1,34 @@
 const Dog = document.getElementById('dog');
 const cat = document.getElementById('cat');
 
-Dog.addEventListener('click', ()=>{
-    document.getElementById('catcal').style.display = none;
-    document.getElementById('dogcal').style.display = block;
-});
+// Function to toggle the menu
+  function toggleMenu() {
+    const menu = document.querySelector('.slide');
+    menu.style.transform = (menu.style.transform === "translateX(0%)" || menu.style.transform === "") ? "translateX(-100%)" : "translateX(0%)";
+  }
 
-cat.addEventListener('click', ()=>{
-    document.getElementById('catcal').style.display = block;
-    document.getElementById('dogcal').style.display = none;
-})
+  // Swipe detection
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  document.addEventListener('touchstart', function (event) {
+    touchStartX = event.touches[0].clientX;
+  });
+
+  document.addEventListener('touchend', function (event) {
+    touchEndX = event.changedTouches[0].clientX;
+    handleSwipe();
+  });
+
+  function handleSwipe() {
+    const swipeThreshold = 50; // Adjust this value as needed
+
+    if (touchStartX - touchEndX > swipeThreshold) {
+      // Swipe to the left (closing menu)
+      toggleMenu();
+    } else if (touchEndX - touchStartX > swipeThreshold) {
+      // Swipe to the right (opening menu)
+      toggleMenu();
+    }
+  }
+  console.log('slide effective')
